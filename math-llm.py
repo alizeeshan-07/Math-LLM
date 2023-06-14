9bd2e6df583f5cdb9547ab8f545ef6081e755f0b
Ali Zeeshan <70628992+alizeeshan-07@users.noreply.github.com>
2023-06-15 02:11:00 +0500

    Update math-llm.py

--- math-llm.py
+++ math-llm.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from langchain import OpenAI, LLMMathChain
 
-llm = OpenAI(openai_api_key="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", temperature=0)
+llm = OpenAI(openai_api_key="KEY", temperature=0)
 llm_math = LLMMathChain.from_llm(llm, verbose=True)
 
 
